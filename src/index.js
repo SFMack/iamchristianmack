99198d5eb1069d5e29a498710185136af98bdf9a
SFMack <sfcamack@icloud.com>
2020-08-15 18:16:17 -0700

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./App.scss";
+import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
