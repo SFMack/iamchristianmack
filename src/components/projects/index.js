0bd3845a2b8e80983b883f13661708af3fc36c05
SFMack <sfcamack@icloud.com>
2020-08-17 20:57:27 -0700

    added carousel to the projects view using placeholder image. need to layout/add skills, contact, about views.

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,9 +1,29 @@
 import React from "react";
 
+import Carousel from "react-bootstrap/Carousel";
+
+import CarouselItems from "../../util/carousel-helper";
+
 const Projects = () => {
   return (
     <div className="content">
-      <p>PROJECTS PAGE</p>
+      <Carousel>
+        {CarouselItems.map((item) => (
+          <Carousel.Item>
+            <img
+              className="d-block m-auto"
+              src={item.imageSrc}
+              alt={item.altText}
+            />
+            <Carousel.Caption>
+              <h3>{item.projectName}</h3>
+              <p>{item.projectDescription}</p>
+            </Carousel.Caption>
+            <button>Code</button>
+            <button>github</button>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </div>
   );
 };
---
+++ src/util/carousel-helper.js
@@ -0,0 +1,30 @@
+// passing an array of objects to projects component
+// to control carousel items dynamically
+const CarouselItems = [
+  {
+    imageSrc: "https://via.placeholder.com/500x300",
+    projectName: "Project 1",
+    projectDescription: "Here is my lengthy project description",
+    altText: "First slide",
+    githubLink: "https://www.google.com",
+    srcCodeLink: "https://www.github.com/SFMack",
+  },
+  {
+    imageSrc: "https://via.placeholder.com/300",
+    projectName: "Project 2",
+    projectDescription: "Here is my lengthy project description",
+    altText: "Second slide",
+    githubLink: "https://www.google.com",
+    srcCodeLink: "https://www.github.com/SFMack",
+  },
+  {
+    imageSrc: "https://via.placeholder.com/300",
+    projectName: "Project 3",
+    projectDescription: "Here is my lengthy project description",
+    altText: "Third slide",
+    githubLink: "https://www.google.com",
+    srcCodeLink: "https://www.github.com/SFMack",
+  },
+];
+
+export default CarouselItems;
