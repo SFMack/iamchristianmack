699d85f9d7f28bf6134ab87b4f35caf5900044fb
SFMack <sfcamack@icloud.com>
2020-09-06 17:39:16 -0700

    contact view updates

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -48,13 +48,12 @@ const Contact = () => {
   return (
     <div className="content contact-content">
       <Form onSubmit={submitForm}>
-        <Row>
+        <Row className="form--top-row">
           <Col>
             <Form.Group controlId="form-name">
-              <Form.Label>Name</Form.Label>
+              <Form.Label>NAME</Form.Label>
               <Form.Control
                 name="name"
-                size="lg"
                 type="text"
                 placeholder="Put your name..."
                 onChange={(e) => setName(e.target.value)}
@@ -64,10 +63,9 @@ const Contact = () => {
 
           <Col>
             <Form.Group controlId="form-email">
-              <Form.Label>Email</Form.Label>
+              <Form.Label>EMAIL</Form.Label>
               <Form.Control
                 name="email"
-                size="lg"
                 type="email"
                 placeholder="Your email here.."
                 onChange={(e) => setEmail(e.target.value)}
@@ -76,13 +74,12 @@ const Contact = () => {
           </Col>
         </Row>
 
-        <Row>
+        <Row className="form--bottom-row">
           <Col>
             <Form.Group controlId="form-message">
-              <Form.Label>Message</Form.Label>
+              <Form.Label>MESSAGE</Form.Label>
               <Form.Control
                 name="message"
-                rows="6"
                 type="textarea"
                 placeholder="Then a message here...please."
                 onChange={(e) => setMessage(e.target.value)}
--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const del = require("del");
 
+// convert the App.scss file to css using the 'sass' method
+// put the new file in css/
 gulp.task("styles", () => {
   return gulp
     .src("./App.scss")
@@ -9,6 +11,7 @@ gulp.task("styles", () => {
     .pipe(gulp.dest("./css/"));
 });
 
+// used to delete the App.css file before compiling the new styles
 gulp.task("clean", () => {
   return del(["css/App.css"]);
 });
